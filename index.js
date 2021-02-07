d0cf45f02b82dc359631d034a72737e6d0de49f3
massyao <massyao@126.com>
2021-02-07 10:55:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 import 'core-js/es6/typed';
-//import 'core-js/fn/object/define-property';
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -9,7 +8,6 @@ import "./scss/index.scss";
 
 import Vaccine_main_content from "./js/components/vaccine-main-content.js";
 
-//ReactDOM.render(
 ReactDOM.render(
     <Vaccine_main_content/>,
     document.getElementById("content")
